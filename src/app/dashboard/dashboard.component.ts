ed217202b227c26f57d6121c3198c2074f5afd4e
Eric Ballou <eballou18@gmail.com>
2023-04-24 21:22:06 -0400

    adding new component

---
+++ src/app/dashboard/dashboard.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnDestroy, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.less']
+})
+
+/**
+ * Dashboard Component
+ */
+export class DashboardComponent implements OnInit, OnDestroy {
+
+
+  constructor() { 
+
+  }
+
+  /**
+   * ngOnInit
+   */
+  ngOnInit() {
+
+  }
+
+  /**
+   * ngOnDestroy
+   */
+  ngOnDestroy() {
+
+  }
+
+}
